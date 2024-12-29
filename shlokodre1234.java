c3d10e1862ddfcb9e7a46ffb3237fc46d30fd24a
Shlok <shlokodre@gmail.com>
2024-12-29 15:41:21 +0530

    bhand

---
+++ shlokodre1234.java
@@ -0,0 +1,8 @@
+public class shlokodre1234{
+    public static void main(String[]args){
+        System.out.println("hello moglas");
+        System.out.println(" ");
+        System.out.println("");
+    }
+
+}
